31fad6f081df1826a73e099c3f3359b68dd660a9
aditid29 <92687234+aditid29@users.noreply.github.com>
2021-10-18 09:51:19 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,6 +3,11 @@ import java.util.*;
 import java.util.ArrayList;
 
 public class Main{
+    double totalCost;
+    Order order;
+    Product product;
+    double q;
+
     public static void main(String[] args){
         System.out.println("Welcome To Daily Fresh");
         System.out.println("Please enter your details:");
@@ -14,12 +19,13 @@ public class Main{
         System.out.println("Address:");
         String address = sc.nextLine();
         Customer c1 = new Customer(address,name,phoneNo);
-        System.out.println(c1.getName());
-        System.out.println(c1.getPhoneNo());
-        System.out.println(c1.getAddress());
 
-        System.out.println("Hello," +c1.getName()+ " "+"what would you like to buy today?");
-        System.out.println("1.Milk\n2.Eggs\n3.Cheese\n4.Butter\n5.Bread");
+        System.out.println("Name: "+c1.getName());
+        System.out.println("Mobile No:" +c1.getPhoneNo());
+        System.out.println("Address:" +c1.getAddress());
+
+        System.out.println("Hello," +c1.getName()+ " "+" Here's a list of products and their price");
+        //System.out.println("1.Milk\n2.Eggs\n3.Cheese\n4.Butter\n5.Bread");
         ArrayList<Product> cart = new ArrayList<Product>();
         Product p1 = new Product("Milk" ,"1 ltr",50 );
         cart.add(p1);
@@ -31,21 +37,50 @@ public class Main{
         cart.add(p4);
         Product p5 = new Product("Butter", "100 gm", 50);
         cart.add(p5);
-
-
-
-
-
-
-
-
-        /*for(int i=0; i < cart.size();i++) {
+        for(int i=0; i < cart.size();i++) {
             System.out.println(cart.get(i));
-        }*/
-        System.out.print(cart);
+        }
+        System.out.println("What would you like to buy ?");
+        String item = sc.nextLine();
+        System.out.println("Enter quantity");
+        double q = sc.nextDouble();
+
+
+        if (q > 5){
+            System.out.println("Maximum 5 items allowed per order");
+        }
+        else {
+        switch (item) {
+            case "Milk":
+
+
+
+
+
+        }
+        }
+        
+
+
+
+
+
+
+
+
+
+        //System.out.print(cart);
 
         sc.close();
 
     }
 
+
+    public int getTotalCost(double q,Product product) {
+        return q*prodMrp;
+    }
+
+    /*public void setTotalCost(int totalCost) {
+        this.totalCost = totalCost;
+    }*/
 }
